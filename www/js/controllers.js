1cebd528c20e5042b460fc762dc7601338ef6f80
TranslateThis <Translate_This@outlook.com>
2016-06-21 13:40:02 -0300

    Build0026

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,5 +1,6 @@
-angular.module('app.controllers', [])
-  
+angular.module('app.controllers', ['timer'])
+
+//CONTROLADOR DEL REPRODUCTOR   
 .controller('reproducirCtrl', function($scope) {
 $scope.images = [];
 
@@ -14,109 +15,185 @@ $scope.loadImages = function(){
 		else{
 			$scope.images.push({id:i, src:"img/vanhalen.jpg"});
 		}
-		
 	};
 }
 
 })
    
-.controller('grabarAudioCtrl', function($scope) {
-  var audio =null;
+//CONTROLADOR DE GRABAR AUDIO     
+.controller('grabarAudioCtrl', function($scope, $ionicPopup) {
+  $scope.state = "record";
+  $scope.audioFormat = "wav";
+  var audio;
+  
 
-	//Capturo audio 
+
+  $scope.start = function () {
+    $scope.$broadcast('timer-start');
+  };
+  $scope.stop = function () {
+    $scope.$broadcast('timer-stop');
+  };
+
+  //
+  $scope.updateSelection = function(position, items, title) {
+        angular.forEach(items, function(subscription, index) {
+            if (position != index)
+                subscription.checked = false;
+                $scope.selected = title;
+            }
+        );
+    }
+
+  //Selección de formato de Grabación de un Audio 
+  $scope.formatos = function(){
+      $scope.devList = [{ text: "Amr", checked: false },{ text: "Mp3", checked: false },{ text: "M4a", checked: false },{ text: "Wav", checked: true  },{ text: "Wma", checked: false }];
+
+      var prueba = '<ion-checkbox class="checkbox-assertive checkbox-circle" ng-repeat="item in devList" ng-model="item.checked" ng-checked="item.checked" ng-click="updateSelection($index, devList, item.text)">{{ item.text }}</ion-checkbox>'
+
+    // Popup para seleccionar formáto de grabación
+    var myPopup = $ionicPopup.show({
+      template: prueba,
+      title: 'Seleccione Formato de Grabación',
+      subTitle: 'Recuerde comprobar la compatibilidad de su equipo móvil (Por defecto se recomienda Wav)',
+      scope: $scope,
+      buttons: [
+        { text: 'Cerrar' },
+        {
+          text: '<b>Confirmar</b>',
+          type: 'button-assertive',
+          onTap: function(e) {
+            for (var k in $scope.devList){
+              if ($scope.devList.hasOwnProperty(k) && $scope.devList[k].checked == true ) {
+                $scope.audioFormat = $scope.devList[k].text;
+              }
+            }
+          }
+        }
+      ]
+    });
+  }
+
+	//Grabación de un Audio 
 	$scope.capturarAudio = function(){
-    var extension = ".wav";
-    filepart = new Date();
-    var day = filepart.getDate().toString();
-    var month = (filepart.getMonth()+1).toString();
-    var year = filepart.getFullYear().toString();
-    var hours = filepart.getHours().toString();
-    var minutes = filepart.getMinutes().toString();
-    var seconds = filepart.getSeconds().toString();
-    var filename = day + "-" + month + "-" + year + "_" + hours + ":" + minutes + ":" + seconds;
-    var src = cordova.file.externalRootDirectory + "/sonidosproa/" + filename + extension;
-
-    audio = new Media(src, function(e){console.log(e,"success");}, function(e){console.log(e,"error");});
-    audio.startRecord();
+		$scope.state = "recording";
+   		var extension = "." + $scope.audioFormat;
+   		filepart = new Date();
+   		var day = filepart.getDate().toString();
+   		var month = (filepart.getMonth()+1).toString();
+   		var year = filepart.getFullYear().toString();
+   		var hours = filepart.getHours().toString();
+   		var minutes = filepart.getMinutes().toString();
+   		var seconds = filepart.getSeconds().toString();
+   		var sep = "-";
+   		var filename = day + sep + month + sep + year + sep + hours + sep + minutes + sep + seconds + extension;
+   		var src = cordova.file.externalRootDirectory + "/sonidosproa/" + filename;
+   		audio = new Media(src, function(e){console.log(e,"success");}, function(e){console.log(e,"error");});
+   		audio.startRecord();
 	}
 
+  //Freno la grabación del Audio
 	$scope.pararAudio = function(){
-    audio.stopRecord();
-    audio.release();
-    audio=null;
+    	audio.stopRecord();
+    	audio.release();
+    	//var str = JSON.stringify(audio, null, 4);
+    	$scope.state = "record";
+    	window.plugins.toast.showWithOptions({
+    		message: "El audio capturado a sido agregado a la biblioteca de Grabaciones.",
+    		duration: 5000, // 5000 ms
+    		position: "top",
+    		styling: {
+      			opacity: 0.75, // 0.0 (transparent) to 1.0 (opaque). Default 0.8
+      			backgroundColor: '#333333', // make sure you use #RRGGBB. Default #333333
+      			textColor: '#FFFFFF', // Ditto. Default #FFFFFF
+      			textSize: 20.5, // Default is approx. 13.
+      			cornerRadius: 16, // minimum is 0 (square). iOS default 20, Android default 100
+      			horizontalPadding: 20, // iOS default 16, Android default 50
+      			verticalPadding: 16 // iOS default 12, Android default 30
+    		}
+  		});
 	}
-
 
 })
-   
+
+//CONTROLADOR DE LAS GRABACIONES   
 .controller('grabacionesCtrl', function($scope) {
 
-    var onSuccessCallback = function(entries){
-      //var str = JSON.stringify(entries, null, 4);
+  var my_media;
+  var onSuccessCallback = function(entries){
+    //var str = JSON.stringify(entries, null, 4);
+    $scope.files = entries;
+    $scope.$apply();
+  }
 
-      $scope.files = entries;
-      $scope.$apply();
-    }
+  var onFailCallback = function(){
+  // In case of error
+  }
 
-    var onFailCallback = function(){
-    // In case of error
-    }
+  var myPath = cordova.file.externalRootDirectory + "/sonidosproa/";
+  window.resolveLocalFileSystemURL(myPath, function (dirEntry) {
+    var directoryReader = dirEntry.createReader();
+    directoryReader.readEntries(onSuccessCallback,onFailCallback);
+  });      
 
-    var myPath = cordova.file.externalRootDirectory + "/sonidosproa/";
+  //Reproduzco un audio grabado determinado
+  $scope.playRecordedAudio = function(name){
+    my_media = new Media(cordova.file.externalRootDirectory + "/sonidosproa/" + name, function(e) { 
+    my_media.release();
+    }, function(err) {
+      console.log("media err", err);
+    });
+    my_media.play();
+  }
+
+  var onResolveSuccess = function(fileEntry){
+    fileEntry.remove();
     window.resolveLocalFileSystemURL(myPath, function (dirEntry) {
-      var directoryReader = dirEntry.createReader();
-      directoryReader.readEntries(onSuccessCallback,onFailCallback);
-    });      
+    var directoryReader = dirEntry.createReader();
+    directoryReader.readEntries(onSuccessCallback,onFailCallback);
+    });
+    window.plugins.toast.showWithOptions({
+    	message: "Se a eliminado correctamente el Audio " + fileEntry.name,
+    	duration: 6000, // 5000 ms
+    	position: "top",
+    	styling: {
+    		opacity: 0.75, // 0.0 (transparent) to 1.0 (opaque). Default 0.8
+    		backgroundColor: '#333333', // make sure you use #RRGGBB. Default #333333
+    		textColor: '#FFFFFF', // Ditto. Default #FFFFFF
+      		textSize: 20.5, // Default is approx. 13.
+    		cornerRadius: 16, // minimum is 0 (square). iOS default 20, Android default 100
+    		horizontalPadding: 20, // iOS default 16, Android default 50
+    		verticalPadding: 16 // iOS default 12, Android default 30
+    	}
+  	}); 
+  }
 
-    //reproduzco un audio grabado determinado
-    $scope.playRecordedAudio = function(name){
-
-        my_media = new Media(cordova.file.externalRootDirectory + "/sonidosproa/" + name, function(e) { 
-          my_media.release();
-        }, function(err) {
-          console.log("media err", err);
-        });
-        my_media.play();
+  var fail = function(evt){
+    console.log(evt.target.error.code);
+  }
 
 
+  function onConfirmDltRecordedAudio(buttonIndex, name) {
+    if(buttonIndex == '1'){  //se confirma la eliminación del audio
+      window.resolveLocalFileSystemURL(cordova.file.externalRootDirectory + "/sonidosproa/" + name, onResolveSuccess, fail);
     }
+  }
 
-    var onResolveSuccess = function(fileEntry){
-      
-      fileEntry.remove();
-      window.resolveLocalFileSystemURL(myPath, function (dirEntry) {
-      var directoryReader = dirEntry.createReader();
-      directoryReader.readEntries(onSuccessCallback,onFailCallback);
-      }); 
-    }
+  //Freno la reproducción de un audio
+  $scope.stopRecordedAudio = function(){
+    my_media.stop();
+  }
 
-    var fail = function(evt){
-      alert(evt.target.error.code);
-    }
-
-
-    function onConfirmDltRecordedAudio(buttonIndex, name) {
-      if(buttonIndex == '1'){  //se confirma la eliminación del audio
-        //window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, onFileSystemSuccess, fail);
-        alert(cordova.file.externalRootDirectory + "/sonidosproa/" + name);
-        window.resolveLocalFileSystemURL(cordova.file.externalRootDirectory + "/sonidosproa/" + name, onResolveSuccess, fail);
-      }
-    }
-
-    $scope.stopRecordedAudio = function(){
-
-    }
-
-    //elimino un audio grabado determinado
-    $scope.deleteRecordedAudio = function(name){
-      navigator.notification.confirm(
-      '¿Desea eliminar este audio?',
-      function(buttonIndex){
-        onConfirmDltRecordedAudio(buttonIndex, name);
-      },
-      'Eliminando Audio',           
-      ['Aceptar','Cancelar']     
-      );
-    }
+  //Elimino un audio grabado determinado
+  $scope.deleteRecordedAudio = function(name){
+    navigator.notification.confirm(
+    '¿Desea eliminar este audio?',
+    function(buttonIndex){
+      onConfirmDltRecordedAudio(buttonIndex, name);
+    },
+    'Eliminando Audio',           
+    ['Aceptar','Cancelar']     
+    );
+  }
 })
        
